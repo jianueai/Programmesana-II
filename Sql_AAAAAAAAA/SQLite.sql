f769bc540ea907364290bdf06877a74505005812
Jianueai <104585173+jianueai@users.noreply.github.com>
2022-12-07 11:59:30 +0200

    Add files via upload

---
+++ Sql_AAAAAAAAA/SQLite.sql
@@ -0,0 +1,70 @@
+DROP table IF EXISTS demo;
+DROP table IF EXISTS Klients;
+DROP table IF EXISTS Produkts;
+DROP table IF EXISTS
+DROP table IF EXISTS Klients;
+
+Create TABLE Klients(
+  Klienta_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+  Vards varchar(30) NOT NULL,
+  Uzvards varchar(30) not NULL,
+  Adrese varchar(100) NOT NULL,
+  E_pasts varchar(100) Not NULL,
+  Parole varchar(30) not NULL,
+  Telefona_nr varchar(14),
+  
+  UNIQUE(Telefona_nr),
+  UNIQUE(E_pasts),
+  UNIQUE(Parole)
+);
+
+CREATE table Produkts(
+  Produkta_ID INTEGER PRIMARY key AUTOINCREMENT,
+  Nosaukums varchar varchar(100) NOT NULL,
+  Skaits INTEGER,
+  Cena FLOAT NOT NULL,
+  
+  UNIQUE(Nosaukums)
+);
+  
+CREATE table Grafiks(
+  Grafika_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+  Diena INTEGER NOT NULL,
+  Mainas_Sakums varchar(5),
+  Mainas_Beigas varchar(5)
+ );
+ 
+ Create table Darbinieks(
+   Darbinieka_ID INTEGER PRIMARY key AUTOINCREMENT,
+   Grafika_ID INTEGER,
+   Vards varchar(30) NOT NULL,
+   Uzvards varhcar(30) NOT NULL,
+   Darbinieka_Telefona_nr varchar(14) NOT NULL,
+   
+   UNIQUE(Darbinieka_Telefona_nr),
+   FOREIGN key(Grafika_ID) REFERENCES Grafiks(Grafika_ID)
+  );
+  
+  CREATE TABLE Pasutijums(
+    Pasutijuma_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    Produkta_ID INTEGER,
+    Darbinieka_ID INTEGER,
+    Klienta_ID INTEGER,
+    Preces_sanemsana varchar(7) ,
+    
+    FOREIGN key(Produkta_ID) REFERENCES Produkts(Produkta_ID),
+    FOREIGN key(Darbinieka_ID) REFERENCES Darbinieks(Darbinieka_ID),
+    FOREIGN key(Klienta_ID) REFERENCES Klients(Klienta_ID)
+   );
+ 
+ CREATE TABLE Grozs(
+   Groza_ID INTEGER PRIMARY key AUTOINCREMENT,
+   Produkta_ID INTEGER,
+   Pasutijuma_ID INTEGER,
+   
+   FOREIGN key(Produkta_ID) REFERENCES Produkts(Produkta_ID),
+   FOREIGN KEY(Pasutijuma_ID) REFERENCES Pasutijums(Pasutijuma_ID)
+ );
+
+  
+  
